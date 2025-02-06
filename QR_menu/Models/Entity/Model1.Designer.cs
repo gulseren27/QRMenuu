e74df8a89645be8a3278a44bf71e9b096d90229b
Gülseren Zamir <86431567+gulseren27@users.noreply.github.com>
2025-02-06 23:57:09 +0300

    commit

--- QR_menu/Models/Entity/Model1.Designer.cs
+++ QR_menu/Models/Entity/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Gulseren\source\repos\QR_menu\Models\Entity\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Gulseren\Source\Repos\QRMenuu\QR_menu\Models\Entity\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
